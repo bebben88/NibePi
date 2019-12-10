841cca54793f9a6441873abe28cd828beea9dce8
bebben88 <32220954+bebben88@users.noreply.github.com>
2019-12-10 22:44:48 +0100

    Update nibepi.sh

--- update/nibepi.sh
+++ update/nibepi.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo "Test download."
-echo -n "Printing text without newline"
-echo -e "\nRemoving \t backslash \t characters\n"
+echo "Starting Update of NibePi"
+echo "Setting R/W mode for the filesystem during update..."
+mount=$(sudo mount -o remount,ro / 2>/tmp/tar_stderr);
+stderr_var=$( cat /tmp/tar_stderr )
+if [[ ($stderr_var == "mount: / is busy") ]]
+then
+    echo "Filesystem is busy"
+else
+echo "${stderr_var}"
+fi
+echo "Looking for Node-RED folder."
+dirNode=$(find / -type f -name 'flows.json' 2>/dev/null | sed -r 's|/[^/]+$||' |sort |uniq)
+echo "${dirNode}"
+cd $dirNode && ls -l
