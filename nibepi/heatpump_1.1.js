6249a99f5133888ef0b7f2745dd7269de89392db
bebben88 <32220954+bebben88@users.noreply.github.com>
2020-05-04 13:03:15 +0200

    Update heatpump_1.1.js

--- nibepi/heatpump_1.1.js
+++ nibepi/heatpump_1.1.js
@@ -1,30 +1,39 @@
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var http = require('https');
 var fs = require('fs');
 
 var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
+  exec(`sudo mount -o remount,rw`, function(error, stdout, stderr) {
+    var file = fs.createWriteStream(dest);
+    var request = http.get(url, function(response) {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close(cb);  // close() is async, call cb after close completes.
+      });
+    }).on('error', function(err) { // Handle errors
+      fs.unlink(dest); // Delete the file async. (But we don't check the result)
+      if (cb) cb(err.message);
     });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
   });
+  
 };
-download("https://raw.githubusercontent.com/anerdins/nibepi-flow/master/update/upgrade_test.js","/tmp/upgrade.js", function(result) {
-  if(result!==undefined) {
-    download("https://raw.githubusercontent.com/anerdins/nibepi-flow/master/update/upgrade_test.sh","/tmp/upgrade.sh", function(result) {
-  if(result!==undefined) {
-    const child = spawn('node', ['/tmp/upgrade.js'], {
-      env: { data: "Hello" },
-      detached: true,
-      stdio: 'inherit'
+download("https://raw.githubusercontent.com/bebben88/NibePi/master/update/upgrade_test.js","./upgrade.js", function(result) {
+  if(result===undefined) {
+    exec(`sudo chown ${process.env.USER}:${process.env.USER} ./upgrade.js`, function(error, stdout, stderr) {
+      download("https://raw.githubusercontent.com/bebben88/NibePi/master/update/upgrade_test.sh","/tmp/upgrade.sh", function(result) {
+  if(result===undefined) {
+    exec(`sudo chown ${process.env.USER}:${process.env.USER} /tmp/upgrade.sh && chmod u+x /tmp/upgrade.sh`, function(error, stdout, stderr) {
+      const child = spawn('node', ['./upgrade.js'], {
+        env: { data: "Hello" },
+        detached: true,
+        stdio: 'inherit'
+      });
+      //child.unref();
     });
-    //child.unref();
+    
   }
 })
+  });
   }
 })
